eee42fe1d7761f74b57b437ab25b389fb1acd100
Leo <ferteleo@gmail.com>
2023-07-22 16:39:13 +0200

    responsive home & presta OK

--- components/Prestations.js
+++ components/Prestations.js
@@ -30,26 +30,13 @@ export default function Prestations() {
     setPhotoIndex(newIndex);
   }
 
-  /// sablier ///
+  /// Mobile ///
 
-  const [timerProgress, setTimerProgress] = useState(0);
+  const [mobileScreen, setMobileScreen] = useState();
 
-  function startTimer() {
-    const interval = 10; // Intervalle de temps entre chaque itération (en millisecondes)
-    const totalTime = 9000; // Durée totale du sablier (en millisecondes)
-    const numIterations = totalTime / interval; // Nombre total d'itérations
-
-    let progress = 0; // Progression initiale
-
-    const timer = setInterval(() => {
-      progress += 100 / numIterations; // Augmentation de la progression à chaque itération
-      setTimerProgress(progress);
-
-      if (progress >= 100) {
-        clearInterval(timer); // Arrête le timer lorsque la progression atteint 100%
-      }
-    }, interval);
-  }
+  const calculateScreen = () => {
+    window.innerWidth <= 425 ? setMobileScreen(true) : setMobileScreen(false);
+  };
 
   /// Charger le texte ///
 
@@ -122,7 +109,8 @@ export default function Prestations() {
   useEffect(() => {
     loadContent();
     loadPresentation();
-    startTimer();
+
+    calculateScreen();
     const interval = setInterval(() => {}, 9000);
 
     return () => {
@@ -132,7 +120,7 @@ export default function Prestations() {
 
   /// Envoi du mail ///
 
-  return (
+  return mobileScreen ? (
     <div className={styles.mainContainer}>
       <Cart
         style={displayCart}
@@ -143,6 +131,117 @@ export default function Prestations() {
           messageIsFalse();
         }}
       />
+
+      <div className={styles.menuContainer}>
+        <Menu
+          clickCart={() => {
+            setCartClicked(true);
+          }}
+          display={"none"}
+        />
+      </div>
+      <div className={styles.secondContainer}>
+        {texte && (
+          <div className={styles.middleContainer}>
+            <div className={styles.descriptionContainer}>
+              <div className={styles.titleContainer}>
+                <div className={styles.text}>{premierePartie}</div>
+                <div className={styles.text2}>{secondePartie}</div>
+              </div>
+              <div className={styles.text} style={{ whiteSpace: "pre-line" }}>
+                {texte[0].context}
+              </div>
+            </div>
+
+            <form className={styles.form}>
+              <h3 className={styles.text}>
+                Pour me faire part de vos envies !
+              </h3>
+              <input
+                type="text"
+                className={styles.normalInput}
+                placeholder="PRÉNOM"
+                name="user_firstName"
+              />
+              <input
+                type="text"
+                className={styles.normalInput}
+                placeholder="NOM"
+                name="user_lastName"
+              />
+              <input
+                type="text"
+                className={styles.normalInput}
+                placeholder="MAIL"
+                name="user_email"
+              />
+              <input
+                type="text"
+                className={styles.normalInput}
+                placeholder="TÉLÉPHONE"
+                name="user_phone"
+              />
+
+              <textarea
+                className={styles.messageInput}
+                placeholder="VOTRE MESSAGE"
+                name="message"
+                rows={10}
+              />
+            </form>
+            <div className={styles.sendContainer}>
+              <input
+                type="submit"
+                value="ENVOYER"
+                className={styles.sendButton}
+              />
+            </div>
+          </div>
+        )}
+
+        {textContent[0] && (
+          <div className={styles.photoAreaContainer}>
+            <div className={styles.picContainer}>
+              <div className={styles.calque1}></div>
+              <div className={styles.calque2}>
+                <div
+                  className={styles.previous}
+                  onClick={() => {
+                    previousPhoto();
+                  }}
+                ></div>
+                <div
+                  className={styles.next}
+                  onClick={() => {
+                    nextPhoto();
+                  }}
+                ></div>
+              </div>
+              <Pic2
+                src={textContent[photoIndex].src}
+                width={textContent[photoIndex].width}
+                height={textContent[photoIndex].height}
+                alt={textContent[photoIndex].collection}
+                style={fadeIn}
+                className={styles.test}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  ) : (
+    <div className={styles.mainContainer}>
+      <Cart
+        style={displayCart}
+        isClicked={messageClicked}
+        onClick={() => {
+          setCartClicked(false);
+          setMessageClicked(false);
+          messageIsFalse();
+        }}
+      />
+
       <div className={styles.menuContainer}>
         <Menu
           clickCart={() => {
@@ -152,7 +251,6 @@ export default function Prestations() {
         />
       </div>
 
-      {/* vérifie si les métadatas ont chargé */}
       {texte && (
         <div className={styles.middleContainer}>
           <div className={styles.descriptionContainer}>
@@ -208,19 +306,11 @@ export default function Prestations() {
           </div>
         </div>
       )}
-      {/* vérifie si les métadatas ont chargé */}
+
       {textContent[0] && (
         <div className={styles.photoAreaContainer}>
           <div className={styles.picContainer}>
-            <div className={styles.calque1}>
-              {" "}
-              {/* <div
-                className={styles.sablier}
-                style={{
-                  background: `linear-gradient(to right, black ${timerProgress}%, white 0)`,
-                }}
-              ></div>*/}
-            </div>
+            <div className={styles.calque1}></div>
             <div className={styles.calque2}>
               <div
                 className={styles.previous}
